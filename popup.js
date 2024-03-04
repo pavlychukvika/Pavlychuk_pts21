04454c0450e0bb77597dcd973bee5f567f4875d8
pavlychukvika <pavluchykvkgtl@gmail.com>
2024-03-04 16:16:19 +0200

    add popup

---
+++ popup.js
@@ -0,0 +1,25 @@
+let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
+let popup = document.querySelector('.popup'); // Само окно
+let openPopupBtn = document.querySelectorAll('.open-popup'); // Кнопки для показа окна
+let closePopupBtn = document.querySelector('.close-popup'); // Кнопка для скрытия окна
+
+
+openPopupBtn.forEach((button) => { // Перебираем все кнопки
+    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+        e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+        popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+        popup.classList.add('active'); // И для самого окна
+    })
+});
+
+closePopupBtn.addEventListener('click',() => { // Вешаем обработчик на крестик
+    popupBg.classList.remove('active'); // Убираем активный класс с фона
+    popup.classList.remove('active'); // И с окна
+});
+
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+    if(e.target === popupBg) { // Если цель клика - фот, то:
+        popupBg.classList.remove('active'); // Убираем активный класс с фона
+        popup.classList.remove('active'); // И с окна
+    }
+});
