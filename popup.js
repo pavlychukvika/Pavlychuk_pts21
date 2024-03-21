a656fd347e4bdc1a3b0e580781af4986ad52ea79
pavlychukvika <pavluchykvkgtl@gmail.com>
2024-03-21 10:48:33 +0200

    add img

--- popup.js
+++ popup.js
@@ -1,25 +1,27 @@
-let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
-let popup = document.querySelector('.popup'); // Само окно
-let openPopupBtn = document.querySelectorAll('.open-popup'); // Кнопки для показа окна
-let closePopupBtn = document.querySelector('.close-popup'); // Кнопка для скрытия окна
+let popupBg = document.querySelector('.popup__bg');
+let popup = document.querySelector('.popup');
+let openPopupBtn = document.querySelectorAll('.open-popup');
+let closePopupBtn = document.querySelector('.close-popup');
 
 
-openPopupBtn.forEach((button) => { // Перебираем все кнопки
-    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
-        e.preventDefault(); // Предотвращаем дефолтное поведение браузера
-        popupBg.classList.add('active'); // Добавляем класс 'active' для фона
-        popup.classList.add('active'); // И для самого окна
+openPopupBtn.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+
+        
     })
 });
 
-closePopupBtn.addEventListener('click',() => { // Вешаем обработчик на крестик
-    popupBg.classList.remove('active'); // Убираем активный класс с фона
-    popup.classList.remove('active'); // И с окна
+closePopupBtn.addEventListener('click',() => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
 });
 
-document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
-    if(e.target === popupBg) { // Если цель клика - фот, то:
-        popupBg.classList.remove('active'); // Убираем активный класс с фона
-        popup.classList.remove('active'); // И с окна
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg) {
+        popupBg.classList.remove('active');
+        popup.classList.remove('active');
     }
 });
